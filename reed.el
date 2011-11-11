5aa4e2315cc66a526d965cd7c43e58a8f15fd5a4
Reed Law <reedlaw@gmail.com>
2011-11-11 08:28:43 -0500

    shell switching

--- reed.el
+++ reed.el
@@ -130,16 +130,29 @@
  ;; If there is more than one, they won't work right.
  )
 
-(defun switch-to-terminal ()
-  "Switch to terminal buffer"
-  (interactive)
-  (switch-to-buffer (get-buffer "*terminal*")))
-
 (put 'upcase-region 'disabled nil)
 
 (put 'erase-buffer 'disabled nil)
 
 (put 'narrow-to-region 'disabled nil)
+
+ (global-set-key [f7] 'alt-shell-dwim)
+ (defun alt-shell-dwim (arg)
+   "Run an inferior shell like `shell'. If an inferior shell as its I/O
+ through the current buffer, then pop the next buffer in `buffer-list'
+ whose name is generated from the string \"*shell*\". When called with
+ an argument, start a new inferior shell whose I/O will go to a buffer
+ named after the string \"*shell*\" using `generate-new-buffer-name'."
+   (interactive "P")
+   (let* ((shell-buffer-list
+ 	  (let (blist)
+	     (dolist (buff (buffer-list) blist)
+	       (when (string-match "^\\*shell\\*" (buffer-name buff))
+	 	(setq blist (cons buff blist))))))
+	  (name (if arg
+	 	   (generate-new-buffer-name "*shell*")
+	 	 (car shell-buffer-list))))
+     (shell name)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
